0a52a2bf34516400f91b46591c27594f186c00c1
Martín Vásquez <pedromvasquezb@gmail.com>
2019-02-26 12:06:56 -0500

    Add startup service

---
+++ DPD/app/services/startup-service.js
@@ -0,0 +1,49 @@
+const localStorage = require("nativescript-localstorage");
+const http = require("http");
+
+const stateKey = "dpd-test";
+const API_KEY = 'AIzaSyC3FPTr_N3xBMmSlynSj__nhmOHsFV0JhI';
+
+const FIELD_MAP = {
+    'Nombre': 'name',
+    'Industria': 'industry',
+    'Tipo de solución': 'solution',
+    'País': 'country', 
+    'Fecha de contacto': 'contactDate',
+    'Página web': 'website',
+    'Universidad incubadora': 'incubator',
+};
+
+function handleErrors(e) {
+    console.log(e.message);
+}
+
+function formatJSONResults(values) {
+    let fields = values.splice(0, 1)[0];
+    let startups = values.map((entry) => {
+        let startup = entry.reduce((acc, value, index) => {
+            let fieldName = fields[index];
+            acc[FIELD_MAP[fieldName]] = value;
+            return acc;
+        }, {});
+        // Fill missing field, only trailing columns or rows are omitted
+        startup[FIELD_MAP[fields[fields.length - 1]]] = "";
+        return startup;
+    });
+    return startups;
+}
+
+exports.getSpreadSheetData = (spreadsheetId, range) => {
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${API_KEY}`;
+    
+    return new Promise((resolve, reject) => {
+        http.getJSON(url).then((result) => {
+            let startups = formatJSONResults(result.values);
+            localStorage.setItem(`${stateKey}/startups`);
+            resolve(startups);
+        }).catch((e) => {
+            handleErrors(e);
+            reject(e);
+        });
+    });
+}
